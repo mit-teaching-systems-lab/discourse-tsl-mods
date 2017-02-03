3560fb23b8c9a1129d5eb4f073d29c2764703369
Kevin Robinson <kevin.robinson.0@gmail.com>
2017-02-03 10:39:17 -0500

    Working initial prototype

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # discourse-tsl-mods
 Discourse plugin with mods for TSL's EdX courses
+
+## TODO
+- Form working
+- XHR request and error handling
+- Form conditional only on Groups category
+- Style form, modal?
+- Extract config for magic category
+
+
+## Local development
+You can develop with Vagrant ([see Discourse docs](https://github.com/discourse/discourse/blob/master/docs/VAGRANT.md)).  As you develop, clear the ERB cache, copy this repository to the `plugins` folder and restart Rails to see changes.
+
+Example (also using `discourse-edx-lti`):
+```
+rm -rf tmp/cache && \
+rm -rf ./plugins/discourse-edx-lti/ && \
+rsync -av --exclude .git \
+  ~/github/mit-teaching-systems-lab/discourse-edx-lti \
+  ./plugins/ && \
+rsync -av --exclude .git \
+  ~/github/mit-teaching-systems-lab/discourse-tsl-mods \
+  ./plugins/ && \
+rsync -av --exclude .git \
+  ~/github/Mittineague/discourse-plugin-outlet-locations \
+  ./plugins/ && \
+vagrant ssh -c 'cd /vagrant && bundle exec rails s -b 0.0.0.0'
+```
+
+
+## Cloning a site
+See https://meta.discourse.org/t/move-your-discourse-instance-to-a-different-server/15721/20 or https://meta.discourse.org/t/is-there-any-way-to-restore-your-site-from-backup-in-the-terminal/42377/2
+
+
+Example:
+```
+opts = {
+  filename: "whateveryourforumiscalled-2016-04-12-153600.tar.gz",
+  publish_to_message_bus: true
+}
+
+SiteSetting.allow_restore = true
+BackupRestore.restore!(Discourse.system_user.id, opts)
+```
+
+Another:
+```
+script/discourse restore <filename.of.the.backup.tar.gz>
+```
+
+Note that there are several production settings you might need to change, like
+backups to S3 buckets, and HTTPS only.
+
+- Files > S3 keys
+- Backups > Disable S3 backups
+
+
+```
+WARNING: force_https is enabled in dev
+It is very unlikely you are running HTTPS in dev.
+Without HTTPS your session cookie will not work
+Try: bin/rails c
+SiteSetting.force_https = false
+```
---
+++ plugin.rb
@@ -0,0 +1,78 @@
+# ---------------------------------------------------------------
+# name:  discourse-tsl-mods
+# about: Discourse plugin with mods for TSL's EdX courses Edit
+# version: 0.0.1
+# author: MIT Teaching Systems Lab
+# url: https://github.com/mit-teaching-systems-lab/discourse-tsl-mods
+# required_version: 1.8.0.beta4
+# ---------------------------------------------------------------
+
+# This uses the `discovery-list-container-top` plugin outlet to 
+# inject a form that allows the user to create a special kind of Group.
+
+# TODO(kr)
+# Enable site settings in admin UI.
+# See descriptions in files in the `config` folder.
+# enabled_site_setting :learner_groups_category_name
+
+
+# This adds in a new method for creating categories.
+# It can only create categories with the parent category "Groups",
+# doesn't allow any other configuration (eg., permissions),
+# and allows any user to create a category like this.
+after_initialize do
+  Discourse::Application.routes.append do
+    post 'category/group' => 'categories#create_group'
+  end
+
+  CategoriesController.class_eval do
+    def create_group
+      log :info, "CategoriesController#create_group"
+      # TODO(kr) allow SiteSetting, not sure why this doesn't work
+      # group_category_name = SiteSetting.learner_groups_category_name
+
+      # Check for special "Groups" category first
+      group_category_name = 'Groups'
+      group_category = Category.find_by_name(group_category_name)
+      if not group_category
+        log :info, "no group_category"
+        return render status: 404, json: { error: "Learner groups category name not found: #{group_category_name}" }
+      end
+
+      # Create the category, limiting the params
+      # This is adapted from CategoriesController#create
+      group_category_params = params_for_group_category(group_category.id, category_params, current_user)
+      log :info, "group_category_params: #{group_category_params.inspect}"
+      @category = Category.create(group_category_params)
+      if @category.save
+        log :info, "username: #{current_user.username} created category: #{@category.name}"
+        # render_serialized(@category, CategorySerializer)
+        redirect_to '/c/groups'
+      else
+        log :info, "render_json_error"
+        # return render_json_error(@category) unless @category.save
+        redirect_to '/c/groups?create_group_category_error'
+      end
+    end
+
+    private
+    # Fix some parameters and limit what can be changed by the user
+    def params_for_group_category(group_category_id, params, user)
+      user_params = params.slice(:name, :color, :text_color, :topic_template)
+      locked_params = {
+        permissions: {
+          everyone: 1
+        },
+        parent_category_id: group_category_id
+      }
+
+      user_params.merge(locked_params).merge(user: user)
+    end
+
+    def log(method_symbol, text)
+      msg = "CategoriesController#create_group, #{text}"
+      Rails.logger.error(msg)
+      puts msg
+    end
+  end
+end
